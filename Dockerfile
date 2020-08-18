b5568c1aea2086f60610dd48fc814655fdb6cdc7
Drew Tayman <dretay@gmail.com>
2020-08-18 11:38:58 -0400

    misc updates

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,36 @@
 # pull official base image
 FROM node:13.12.0-alpine
 
+
+# needed to build gpy'd dependencies
+RUN apk add --no-cache --virtual .gyp \
+        python \
+        make \
+        g++
+
+
 RUN apk add --no-cache openssl
 ENV DOCKERIZE_VERSION v0.6.1
 RUN wget https://github.com/jwilder/dockerize/releases/download/$DOCKERIZE_VERSION/dockerize-alpine-linux-amd64-$DOCKERIZE_VERSION.tar.gz \
     && tar -C /usr/local/bin -xzvf dockerize-alpine-linux-amd64-$DOCKERIZE_VERSION.tar.gz \
     && rm dockerize-alpine-linux-amd64-$DOCKERIZE_VERSION.tar.gz
 
-# set working directory
-WORKDIR /app
+RUN mkdir /bin/www
+WORKDIR /bin/www
+COPY package.json ./
+COPY package-lock.json ./
+RUN chown -R node:node /bin/www
 
 # add `/app/node_modules/.bin` to $PATH
 ENV PATH /app/node_modules/.bin:$PATH
 
 # install app dependencies
-COPY package.json ./
-COPY package-lock.json ./
-RUN npm install --silent
+USER node
+RUN npm install
 
-# add app
+
+#add app
 COPY . ./
 
-# start app
-CMD ["npm", "start"]
+# let's fly
+CMD [ "npm", "start" ]
--- app.js
+++ app.js
@@ -1,15 +1,68 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
+const AWS = require("aws-sdk");
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
+app.get("/list", function (req, res) {
+	AWS.config.update({
+		accessKeyId: "0000",
+		secretAccessKey: "0000",
+		region: "us-east-2",
+		endpoint: "http://localstack:4566",
+	});
+	const dynamoDb = new AWS.DynamoDB.DocumentClient();
+	const params = {
+		TableName: "processimage-dev",
+	};
+	dynamoDb.scan(params, (error, result) => {
+		// handle potential errors
+		if (error) {
+			console.error(error);
+			res.send("Couldn't fetch ");
+			return;
+		}
+
+		res.send(JSON.stringify(result.Items));
+	});
 });
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("/ping", function (req, res) {
+	// AWS.config.update({
+	// 	accessKeyId: "0000",
+	// 	secretAccessKey: "0000",
+	// 	region: "us-east-2",
+	// 	endpoint: "http://localstack:4566",
+	// });
+	// const dynamoDb = new AWS.DynamoDB.DocumentClient();
+	// const params = {
+	// 	TableName: process.env.DYNAMODB_TABLE,
+	// };
+	// dynamoDb.scan(params, (error, result) => {
+	// 	// handle potential errors
+	// 	if (error) {
+	// 		console.error(error);
+	// 		callback(null, {
+	// 			statusCode: error.statusCode || 501,
+	// 			headers: { "Content-Type": "text/plain" },
+	// 			body: "Couldn't fetch the todos.",
+	// 		});
+	// 		return;
+	// 	}
+
+	// 	// create a response
+	// 	const response = {
+	// 		statusCode: 200,
+	// 		body: JSON.stringify(result.Items),
+	// 	};
+	// 	callback(null, response);
+	// });
+	return res.send("pong?");
 });
 
-app.listen(process.env.PORT || 8080);
+app.get("/", function (req, res) {
+	res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(3000);
